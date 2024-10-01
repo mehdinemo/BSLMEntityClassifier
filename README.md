088af19181969e247fe1d5eea1ed86a08b771a34
mehdinemo <mehdinemati.nmt@gmail.com>
2024-10-01 17:02:58 +0330

    llm: groq

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Basalam project for AI expert interview.
 
 [Main Repo](https://github.com/basalam/entity-classifier)  
 [Dataset Path](https://huggingface.co/datasets/BaSalam/bslm-product-entity-cls-610k)
+
+## Expected Environment Variables
+
+- `OPENAI_API_KEY`
+- `GROQ_API_KEY`
--- investigate_approaches.py
+++ investigate_approaches.py
@@ -1,15 +1,25 @@
 import json
+import os
 from datetime import datetime
 
 import datasets
+from groq import Groq
+from openai import OpenAI
 from pydantic import BaseModel
 
-from openai import OpenAI
 from utils import logger_function
 
 logger = logger_function(name="investigate_approaches")
 
-client = OpenAI()
+model = "llama-3.2-11b-text-preview"  # gpt-4o-mini | llama-3.2-11b-text-preview
+test_dataset_path = "test_ds.json"
+
+logger.info(f"Starting investigate_approaches model: {model}")
+
+if "gpt" in model:
+    client = OpenAI()
+else:
+    client = Groq()
 
 
 class Product(BaseModel):
@@ -24,71 +34,101 @@ Response format:
 {output_schema}
 ```"""
 
+# در مواردی که مدل داده پیچیده است از اسکیما استفاده کنیم
 # output_schema = json.dumps(Product.model_json_schema())
+
+# در این مورد چون تنها یه پارامتر میخایم و وراثت و ساختار پیچیده ای در کار نیست،
+# و با هدف کاهش توکن ورودی یه ساختار خیلی ساده برای اسکیمای خروجی تعین کردیم
 output_schema = '{"entity": "extracted entity"}'
 
 system_content = system_content_template.format(output_schema=output_schema)
 
 
-def zero_shot(user_content: str) -> str | None:
+def zero_shot(user_content: str):
+    completion_kw = dict(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": system_content
+            },
+            {
+                "role": "user",
+                "content": user_content
+            }
+        ],
+        temperature=0
+    )
+
+    if "gpt" in model:
+        completion_kw["response_format"] = {"type": "json_object"}
+
     try:
         st = datetime.now()
-        completion = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_content
-                },
-                {
-                    "role": "user",
-                    "content": user_content
-                }
-            ],
-            temperature=0,
-            response_format={"type": "json_object"}
-        )
+        completion = client.chat.completions.create(**completion_kw)
         duration = (datetime.now() - st).total_seconds()
-        usage = completion.usage.to_dict()
-        usage.pop("completion_tokens_details")
-        usage.update({"duration": duration})
-        logger.info(f"successful completion: {usage}")
     except:
         logger.exception("create completion")
-        return None
+        return None, None
+
+    usage = completion.usage.to_dict()
+    usage.update({"duration": duration})
+    logger.info(f"successful completion: {usage}")
 
     try:
         product = Product.model_validate_json(completion.choices[0].message.content)
     except:
         logger.exception(f"ValidationError for completion content: {completion.choices[0].message.content}")
-        return None
+        return None, usage
 
-    return product.entity
+    return product.entity, usage
 
 
 def add_user_content(products):
+    """
+    اضافه کردن پیام کاربر با فرمت YAML به دیتاست برای کوئری به LLM
+    استفاده نشده چون برای شروع فقط title رو استفاده میکنیم.
+    """
+
     products["user_content"] = f"Product:\n- title: {products['title']}\n- description: {products['description']}"
     return products
 
 
-def main():
+def create_test_ds():
+    """
+    ساخت دیتاست تست با تعداد کم برای تست LLM ها
+    مهمه که مدل ها روی یک دیتاست یکسان تست بشن تا پارامترهای مختلفشون رو مقایسه کنیم.
+    چون در توسعه کد ممکنه مدل های مختلف تست بشن و شرایط مختلفی تاثیر گذاره ترجیحا این دیتاست تست رو روی دیسک ذخیره میکنیم.
+    """
+
     dataset = datasets.load_dataset('BaSalam/bslm-product-entity-cls-610k')
-
-    dataset = dataset["train"].train_test_split(train_size=5, shuffle=True)
-
+    dataset = dataset["train"].train_test_split(test_size=20, shuffle=True)
     # ds["train"] = ds["train"].map(add_user_content, batched=True, batch_size=100)
+    return dataset['test'].to_list()
+
+
+def main():
+    if not os.path.isfile(test_dataset_path):
+        test_ds = create_test_ds()
+        with open(test_dataset_path, 'w') as f:
+            json.dump(test_ds, f, ensure_ascii=False, indent=2)
+    else:
+        with open(test_dataset_path, "r") as f:
+            test_ds = json.load(f)
 
     extracted_entities = []
-    for product in dataset["train"]:
+    for product in test_ds:
+        extracted_entity, usage = zero_shot(product['title'])
         product.update(
             {
-                "extracted_entity": zero_shot(product['title'])
+                "extracted_entity": extracted_entity,
+                "usage": usage
             }
         )
         extracted_entities.append(product)
 
-    with open('openai_res.json', 'w') as f:
-        json.dump(extracted_entities, f, ensure_ascii=False)
+    with open(f'{model}.json', 'w') as f:
+        json.dump(extracted_entities, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
